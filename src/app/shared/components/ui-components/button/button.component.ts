c90836a6ee012119fa22a7a3c5ceb13fd2068fb8
Oleh Leskiv <oleh.leskiv.4@gmail.com>
2019-07-03 00:38:42 +0300

    Fix button text

--- src/app/shared/components/ui-components/button/button.component.ts
+++ src/app/shared/components/ui-components/button/button.component.ts
@@ -11,7 +11,7 @@ import { ViewEncapsulation } from '@angular/core';
 
 export class ButtonComponent {
   @Input() color: string;
-  @Input() type: string = 'raised';
+  @Input() type = 'raised';
   @Input() size: string;
   @Input() disabled: boolean;
   @Input() noHover: boolean;
