3384cd5437408262b5a3c5e46d0ca551fa4e7dba
DennyReM <winter344@gmail.com>
2019-08-15 14:41:33 +0300

    menu ui-change

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -15,13 +15,13 @@ import { trigger, transition, style, animate } from '@angular/animations';
           [
             style({ height: 0, opacity: 0 }),
             animate('0.5s ease-out',
-              style({ height: 200, opacity: 1 }))
+              style({ height: 150, opacity: 1 }))
           ]
         ),
         transition(
           ':leave',
           [
-            style({ height: 200, opacity: 1 }),
+            style({ height: 150, opacity: 1 }),
             animate('0.5s ease-in',
               style({ height: 0, opacity: 0 }))
           ]
