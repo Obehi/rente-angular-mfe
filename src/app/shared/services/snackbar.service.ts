755d7b0890235bf2031ba85fb3eccf4862fd6e56
DennyReM <winter344@gmail.com>
2019-08-16 16:21:15 +0300

    snackbar service forget to add file

---
+++ src/app/shared/services/snackbar.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SnackBarService {
+
+    constructor(private snackBar: MatSnackBar) { }
+
+    public openSuccessSnackBar(message: string) {
+        this.snackBar.open(message, 'Close', {
+            duration: 2 * 1000,
+            panelClass: ['bg-primary'],
+            horizontalPosition: 'right'
+        });
+    }
+
+    public openFailSnackBar(message: string) {
+        this.snackBar.open(message, 'Close', {
+            duration: 2 * 1000,
+            panelClass: ['bg-error'],
+            horizontalPosition: 'right'
+        });
+    }
+}
