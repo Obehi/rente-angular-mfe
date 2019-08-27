58432e543a5d1c5e36aa98e674979f1dfb9eb141
DennyReM <winter344@gmail.com>
2019-08-27 13:12:17 +0300

    set-env js

--- src/set-env.ts
+++ src/set-env.ts
@@ -1,30 +1,25 @@
 const fs = require('fs');
-const argv = require('yargs');
+const path = require('path');
+const environment = process.env.ENV;
+const isProd = environment === 'PROD';
+const targetPath = path.join(__dirname + `/environments/environment.heroku.ts`);
 
-// This is good for local dev environments, when it's better to
-// store a projects environment variables in a .gitignore'd file
-require('dotenv').config();
-
-// Would be passed to script like this:
-// `ts-node set-env.ts --environment=dev`
-// we get it from yargs's argv object
-const environment = argv.environment;
-const isProd = environment === 'prod';
-
-const targetPath = `./src/environments/environment.${environment}.ts`;
+// Setup Heroku configs
 const envConfigFile = `
 export const environment = {
+  name: '${environment}',
   production: ${isProd},
-  baseUrl: "${process.env.BASE_URL}",
-  crawlerUrl: "${process.env.CRAWLER_URL}"
+  baseUrl: '${process.env.BASE_URL}',
+  crawlerUrl: '${process.env.CRAWLER_URL}'
 };
 `;
-console.log(envConfigFile);
 
-fs.writeFile(targetPath, envConfigFile, (err) => {
+fs.writeFile(targetPath, envConfigFile, function (err) {
   if (err) {
-    // console.log(err);
+    console.log(err);
+    return;
   }
 
-  // console.log(`Output generated at ${targetPath}`);
+  console.log(`Environment config generated at ${targetPath}. Environment mode is ${process.env.APP} `);
+  console.log(envConfigFile);
 });
