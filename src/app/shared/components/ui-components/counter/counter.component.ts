68b6b72d56f2e0fcb5b16500663e26bcff2142e6
Bjørn Reitzer <johbjo13@gmail.com>
2019-11-28 22:46:35 +0100

    counter added

---
+++ src/app/shared/components/ui-components/counter/counter.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+
+import { Observable, Observer } from "rxjs";
+
+@Component({
+  selector: "rente-counter",
+  templateUrl: "./counter.component.html",
+  styleUrls: ["./counter.component.scss"]
+})
+export class CounterComponent {
+  public loan: number = 4001753321;
+  public save: number = 150311435;
+  public observable: Observable<boolean>;
+  private observer: Observer<boolean>;
+
+  constructor() {
+    this.observable = new Observable<boolean>(
+      (observer: any) => (this.observer = observer)
+    );
+
+    setTimeout(() => {
+      setInterval(() => {
+        this.loan += 500;
+      }, 2000);
+    }, 1000);
+
+    // For auto mode
+    /* setTimeout(() => (this.number += this.number), 5000); // Update on 5 seconds */
+  }
+}
