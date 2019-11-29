027a22570ed999edd02a2a1a2315f8939f6c06f8
Bjørn Reitzer <johbjo13@gmail.com>
2019-11-30 00:22:46 +0100

    update from feedback

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'rente-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  selector: "rente-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"]
 })
 export class LandingComponent implements OnInit {
-  public isSmallScreen: boolean;
-
-  constructor() {
-    this.onResize();
-  }
-
-  ngOnInit() {
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event?) {
-    window.innerWidth <= 991 ? this.isSmallScreen = true : this.isSmallScreen = false;
-  }
-
+  ngOnInit() {}
 }
