e0bd859393f4e22bcbf1347279d644adf61d9f67
Sergiy Pavlenko <sergey.pavlenko@seniordev.com>
2019-12-04 12:18:20 +0200

    Fix build error

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -2,7 +2,6 @@ import { AuthService } from './../services/remote-api/auth.service';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { ROUTES_MAP } from '@config/routes-config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -13,13 +12,11 @@ export class AuthGuard implements CanActivate {
   }
 
   // TODO: Change to propper config variables
-  canActivate(next: ActivatedRouteSnapshot,
-              state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (this.auth.isLoggedIn) {
       return true;
     }
-
-    this.router.navigate([ROUTES_MAP.bankSelect]);
+    this.router.navigate(['/']);
     return false;
   }
 }
