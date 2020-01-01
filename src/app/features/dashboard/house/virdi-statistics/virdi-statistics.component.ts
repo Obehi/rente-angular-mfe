88876628bc0b04347b56eeb0e337a3e0e28e874f
Sergiy Pavlenko <sergey.pavlenko@seniordev.com>
2020-01-01 15:40:21 +0200

    Fix NPE when calc virdi statistics

--- src/app/features/dashboard/house/virdi-statistics/virdi-statistics.component.ts
+++ src/app/features/dashboard/house/virdi-statistics/virdi-statistics.component.ts
@@ -189,20 +189,24 @@ export class VirdiStatisticsComponent implements OnInit {
 
     this.loansService.getAddressStatistics(this.address.id).subscribe(extendedInfo => {
       this.isLoading = false;
-      this.openMarketSalesHalfYear = extendedInfo.statistics.open_market_sales_6_months;
-      this.indexArea = extendedInfo.indexHistory.area;
-      if (extendedInfo.statistics.price_distribution_sq && extendedInfo.statistics.price_distribution_sq.length > 0) {
-        extendedInfo.statistics.price_distribution_sqm.forEach(element => {
-          this.priceDestributionSqm.push({ from: element.from, to: element.to });
-          this.columnChartOptions.series[0].data.push(element.count);
-          this.columnChartOptions.title.text = `Din kvadratmeterpris: ${extendedInfo.statistics.average_sqm_price} NOK`;
-          this.lineChartOptions.title.text = `Prisutvikling ${extendedInfo.indexHistory.area}`;
-          if (extendedInfo.statistics.average_sqm_price >= element.from && extendedInfo.statistics.average_sqm_price <= element.to) {
-            this.columnChartOptions.plotOptions.column.colors.push('#2b3e50');
-          } else {
-            this.columnChartOptions.plotOptions.column.colors.push('#18BC9C');
-          }
-        });
+      if (extendedInfo.indexHistory && extendedInfo.indexHistory.area) {
+        this.indexArea = extendedInfo.indexHistory.area;
+      }
+      if (extendedInfo.statistics) {
+        this.openMarketSalesHalfYear = extendedInfo.statistics.open_market_sales_6_months;
+        if (extendedInfo.statistics.price_distribution_sqm && extendedInfo.statistics.price_distribution_sqm.length > 0) {
+          extendedInfo.statistics.price_distribution_sqm.forEach(element => {
+            this.priceDestributionSqm.push({ from: element.from, to: element.to });
+            this.columnChartOptions.series[0].data.push(element.count);
+            this.columnChartOptions.title.text = `Din kvadratmeterpris: ${extendedInfo.statistics.average_sqm_price} NOK`;
+            this.lineChartOptions.title.text = `Prisutvikling ${extendedInfo.indexHistory.area}`;
+            if (extendedInfo.statistics.average_sqm_price >= element.from && extendedInfo.statistics.average_sqm_price <= element.to) {
+              this.columnChartOptions.plotOptions.column.colors.push('#2b3e50');
+            } else {
+              this.columnChartOptions.plotOptions.column.colors.push('#18BC9C');
+            }
+          });
+        }
       }
       this.columnChartOptions.xAxis.categories = [...this.createThousandsCategories(this.priceDestributionSqm)];
       const BreakException = {};
