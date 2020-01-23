a68b742f13ca0d5ad3f0a705811e6c5e1a73e334
Sergiy Pavlenko <sergey.pavlenko@seniordev.com>
2020-01-23 21:14:49 +0200

    Add landing page transitions

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'rente-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [
+        style({height:0, opacity:0}),
+        animate('0.5s ease-out', style({height:300, opacity:1}))
+      ]),
+      transition(':leave', [
+        style({height:300, opacity:1}),
+        animate('0.5s ease-in', style({height:0, opacity:0}))
+      ])
+    ])
+  ]
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
 
-  get isMobile(): boolean {
-    return window.innerWidth < 1024;
+  time = 0;
+
+  get isMobile(): boolean { return window.innerWidth < 1024; }
+
+  ngOnInit(): void {
+    const subscription = timer(1000, 1000).subscribe(t => {
+      this.time = t;
+      if (t === 3) {
+        subscription.unsubscribe();
+      }
+    });
   }
 
 }
