2442c007250c77874d210988031453dd8be69b15
Obehi <stian.obehi@gmail.com>
2020-05-30 01:05:47 +0200

    Test variation bug fix

--- src/app/features/dashboard/offers/offers.component.ts
+++ src/app/features/dashboard/offers/offers.component.ts
@@ -418,7 +418,9 @@ export class OffersComponent implements OnInit, OnDestroy {
 
   getVariation(): number | null {
     console.log("variation " + (window as any).google_optimize.get('35KXhAdMSYmOW7kszA1Juw'));
-    return (window as any).google_optimize.get('35KXhAdMSYmOW7kszA1Juw');
+
+    let variation = Number((window as any).google_optimize.get('35KXhAdMSYmOW7kszA1Juw'))
+    return variation;
   }
 
 
