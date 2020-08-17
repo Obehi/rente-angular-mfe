50c66ef2b42d7725a9e0c7f277f62ec8093c3cbf
Obehi <stian.obehi@gmail.com>
2020-08-17 14:04:39 +0200

    --configuration=heroku-dev, sv did not work in post-install.js, reverting changes

--- post-install.js
+++ post-install.js
@@ -4,7 +4,7 @@ let command;
 
 if (process.env.ENV === 'dev') {
     console.log("running heroku dev")
-    command = exec('npm run config-env && ng build --configuration=heroku-dev, sv');
+    command = exec('npm run config-env && ng build --configuration=heroku-dev');
 } else if (process.env.ENV === 'prod') {
     console.log("running heroku prod")
     command = exec('npm run config-env && ng build --configuration=heroku-prod');
