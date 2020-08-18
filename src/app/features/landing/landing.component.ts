fc8bfdc7d55b64351329ecebe6ffb435041997f5
Obehi <stian.obehi@gmail.com>
2020-08-18 12:11:24 +0200

    Add email form to front page

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { timer } from 'rxjs';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { ROUTES_MAP } from '@config/routes-config';
 import {
   FormGroup,
   FormBuilder,
   Validators,
-  FormControl,
-  FormGroupDirective,
+  AbstractControl,
   NgForm,
-  AbstractControl
+  FormControl
 } from "@angular/forms";
 
 import { VALIDATION_PATTERN } from "@config/validation-patterns.config";
@@ -41,6 +41,7 @@ export class LandingComponent implements OnInit {
   public contactUsForm: FormGroup;
   public mask = Mask
   public isLoading: boolean;
+  public emailError: boolean = false
 
   constructor(
     private fb: FormBuilder,
@@ -56,5 +57,73 @@ export class LandingComponent implements OnInit {
         subscription.unsubscribe();
       }
     });
+
+    this.contactUsForm = this.fb.group({
+      email: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(VALIDATION_PATTERN.email)
+        ])
+      ]
+    });
+  }
+
+  isErrorState(
+    control: AbstractControl | null,
+    form: FormGroup | NgForm | null
+  ): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+
+  public sendContactUsForm(formData) {
+    this.isLoading = true;
+    this.contactUsForm.markAllAsTouched();
+    this.contactUsForm.updateValueAndValidity();
+    this.contactService.sendContactForm(formData).subscribe(
+      _ => {
+        this.isLoading = false;
+        this.router.navigate(["/"]);
+        this.snackBar.openSuccessSnackBar("Din melding er sendt", 2);
+      },
+      err => {
+        this.isLoading = false;
+      }
+    );
+  }
+
+  inValid() {
+    return (
+      this.contactUsForm.get('email').hasError('pattern') && 
+      this.contactUsForm.get('email').dirty
+    );
+  } 
+
+  onBlurErrorCheck() {
+    this.emailError = this.inValid()
+   }
+
+  public request() {
+    this.isLoading = true;
+
+    const missingBankData = {
+      email: this.contactUsForm.value.email,
+      bank: "SWEDEN"
+    };
+    
+    this.contactService.sendMissingBank(missingBankData).subscribe(
+      _ => {
+        this.isLoading = false;
+        this.router.navigate(["/"]);
+        this.snackBar.openSuccessSnackBar(
+          "Du får beskjed når din bank er tilgjengelig",
+          1.2
+        );
+      },
+      err => {
+        this.isLoading = false;
+      }
+    );
   }
 }
+
