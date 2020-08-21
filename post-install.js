ebad400d22e5e71a6bf0e4a0d957cf08a8614253
Obehi <stian.obehi@gmail.com>
2020-08-21 15:10:44 +0200

    Add sv to build config

--- post-install.js
+++ post-install.js
@@ -8,7 +8,7 @@ if (process.env.LOCALE == "sv")
 
 if (process.env.ENV === 'dev') {
     console.log("running heroku dev")
-    command = exec('npm run config-env && ng build --configuration=heroku-dev && node move-locale-dir.js');
+    command = exec('npm run config-env && ng build --configuration=heroku-dev' + locale +  '&& node move-locale-dir.js');
 } else if (process.env.ENV === 'prod') {
     console.log("running heroku prod")
     command = exec('npm run config-env && ng build --configuration=heroku-prod');
@@ -17,7 +17,7 @@ if (process.env.ENV === 'dev') {
     command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-dev,sv && node move-locale-dir.js');
 }
 
-console.log("locale enviornment is:" + locale)
+console.log("locale enviornment is:" + process.env.LOCALE)
 
 if (command != undefined) {
     command.stdout.on('data', (data) => {
