56e49ceb8ae523ad152fdd2c0a4080bd5c6bdce6
Obehi <stian.obehi@gmail.com>
2020-08-27 15:31:22 +0200

    Remove locale build option from prod

--- post-install.js
+++ post-install.js
@@ -8,12 +8,14 @@ if (process.env.LOCALE == "sv")
 else if (process.env.LOCALE == "nb")
     locale = ",nb"
     
+console.log("locale is: " + locale);
+
 if (process.env.ENV === 'dev') {
     console.log("running heroku dev")
     command = exec('npm run config-env && ng build --configuration=heroku-dev' + locale );
 } else if (process.env.ENV === 'prod') {
     console.log("running heroku prod")
-    command = exec('npm run config-env && ng build --configuration=heroku-prod' + locale );
+    command = exec('npm run config-env && ng build --configuration=heroku-prod');
 } else {
     console.log("cant find env. running heroku dev")
     command = exec('npm run config-env && ng build --configuration=heroku-dev' + locale );
