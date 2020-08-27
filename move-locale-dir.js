e37b2c2239e6ff0b20071e0b5f7abe7dd6d96ff8
Obehi <stian.obehi@gmail.com>
2020-08-27 13:47:13 +0200

    Add locale login/contact form for login interface

--- move-locale-dir.js
+++ move-locale-dir.js
@@ -2,32 +2,36 @@ var fs = require('fs')
 const path = require('path');
 const { exec } = require('child_process');
 
-let sv =  __dirname + "/dist/rente-front-end/sv";
+
+let locale = "/nb"
+if (process.env.LOCALE === "sv")
+    locale = "/sv"
+else if (process.env.LOCALE === "nb")
+    locale = "/nb"
+
+let localePath =  __dirname + "/dist/rente-front-end" + locale;
 let buffer = __dirname + "/dist/buffer"
 let rentePath = __dirname + "/dist/rente-front-end"
 
-fs.readdir(sv, function (err, files) {
-    //handling error
+fs.readdir(localePath, function (err, files) {
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
-    //listing all files using forEach
     files.forEach(function (file) {
-        // Do whatever you want to do with the file
         console.log(file); 
     });
 });
 
-const basePath = path.resolve(__dirname, '../dist/rente-front-end/');
-const svPath = basePath + "/sv";
+
 
 console.log("moving files")
-console.log(basePath + " <- clientpath");
-console.log(svPath + " <- svPath");
+console.log(localePath + " <- localePath");
+console.log(buffer + " <- bufferPath");
+console.log(rentePath + " <- finalPath");
 
 
 
-fs.renameSync(sv, buffer)
+fs.renameSync(localePath, buffer)
 fs.rmdirSync(rentePath)
 fs.renameSync(buffer, rentePath)
 
--- src/app/features/landing/locale/login/login-sv/login-sv.component.ts
+++ src/app/features/landing/locale/login/login-sv/login-sv.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { VALIDATION_PATTERN } from "@config/validation-patterns.config";
+import { ContactService } from "@services/remote-api/contact.service";
+import { Router } from "@angular/router";
+import { SnackBarService } from "@services/snackbar.service";
+import { Mask } from '@shared/constants/mask'
+
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  NgForm,
+  FormControl
+} from "@angular/forms";
+
 @Component({
   selector: 'rente-login',
   templateUrl: './login-sv.component.html',
@@ -7,9 +22,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginSVComponent implements OnInit {
 
-  constructor() { }
+  public contactUsForm: FormGroup;
+  public mask = Mask
+  public isLoading: boolean;
+  public emailError: boolean = false
+
+  constructor(
+    private fb: FormBuilder,
+    private contactService: ContactService,
+    private router: Router,
+    private snackBar: SnackBarService
+  ) {}
 
   ngOnInit(): void {
+    this.contactUsForm = this.fb.group({
+      email: [
+        ""
+      ]
+    });
   }
 
+  inValid() {
+    return (
+      this.contactUsForm.get('email').hasError('pattern') && 
+      this.contactUsForm.get('email').dirty
+    );
+  } 
+
+  onBlurErrorCheck() {
+    this.emailError = this.inValid()
+   }
+
+  public request() {
+    this.isLoading = true;
+
+    const missingBankData = {
+      email: this.contactUsForm.value.email,
+      bank: "SWEDEN"
+    };
+    
+    this.contactService.sendMissingBank(missingBankData).subscribe(
+      _ => {
+        this.isLoading = false;
+        this.router.navigate(["/"]);
+        this.snackBar.openSuccessSnackBar(
+          "Du får beskjed når din bank er tilgjengelig",
+          1.2
+        );
+      },
+      err => {
+        this.isLoading = false;
+      }
+    );
+  }
+
+  isErrorState(
+    control: AbstractControl | null,
+    form: FormGroup | NgForm | null
+  ): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+
+  public sendContactUsForm(formData) {
+    this.isLoading = true;
+    this.contactUsForm.markAllAsTouched();
+    this.contactUsForm.updateValueAndValidity();
+    this.contactService.sendContactForm(formData).subscribe(
+      _ => {
+        this.isLoading = false;
+        this.router.navigate(["/"]);
+        this.snackBar.openSuccessSnackBar("Din melding er sendt", 2);
+      },
+      err => {
+        this.isLoading = false;
+      }
+    );
+  }
 }
