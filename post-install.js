5291852b7ff7ea3fd2c8086428bbef63bbeaf077
Obehi <stian.obehi@gmail.com>
2020-08-27 12:20:07 +0200

    Add NB support in post-install.js

--- post-install.js
+++ post-install.js
@@ -5,16 +5,19 @@ let command;
 var locale = ""
 if (process.env.LOCALE == "sv")
     locale = ",sv"
+else if (process.env.LOCALE == "nb")
+    locale = ",nb"
+
 
 if (process.env.ENV === 'dev') {
     console.log("running heroku dev")
     command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-dev' + locale +  '&& node move-locale-dir.js');
 } else if (process.env.ENV === 'prod') {
     console.log("running heroku prod")
-    command = exec('npm run config-env && ng build --configuration=heroku-prod');
+    command = exec('npm run config-env && ng build --configuration=heroku-prod'  + locale +  '&& node move-locale-dir.js');
 } else {
     console.log("cant find env. running heroku dev")
-    command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-dev,sv && node move-locale-dir.js');
+    command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-dev' + locale + '&& node move-locale-dir.js');
 }
 
 console.log("locale enviornment is:" + process.env.LOCALE)
