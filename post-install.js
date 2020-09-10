e9aad807b0a2ab8070e47c148d1ad0e53643d004
Obehi <stian.obehi@gmail.com>
2020-09-10 10:25:42 +0200

    Remove locale build option from post-install.js for testing

--- post-install.js
+++ post-install.js
@@ -12,7 +12,7 @@ console.log("locale is: " + locale);
 
 if (process.env.ENV === 'dev') {
     console.log("running heroku dev")
-    command = exec('npm run config-env && ng build --configuration=heroku-dev' + locale);
+    command = exec('npm run config-env && ng build --configuration=heroku-dev');
 } else if (process.env.ENV === 'prod') {
     console.log("running heroku prod")
     command = exec('npm run config-env && ng build --configuration=heroku-prod'+ locale);
