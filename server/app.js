9ce4bc96e585fc426d29c4daea77e4e04c28dda4
Obehi <stian.obehi@gmail.com>
2020-09-14 20:56:01 +0200

    proxy to bank-statistics endpoint is working

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const https = require('https');
 
 const clientPath = path.resolve(__dirname, '../dist/rente-front-end');
 const port = process.env.PORT || 4300;
+const baseUrl = process.env.PORT || 'https://rente-loan-dev.herokuapp.com/';
 
 https.globalAgent.options.ca = require('ssl-root-cas/latest').create();
 
@@ -21,6 +22,28 @@ app.use('/blogg', function(req, res, next) {
       target: 'https://blogg.renteradar.no'
   }, next);
 });
+
+
+
+const historicalRatesProxy = require('http-proxy').createProxyServer({
+  host: 'https://blogg.renteradar.no',
+  changeOrigin: true,
+  agent: new https.Agent({
+    port: 443,
+    auth: 'login:pass'
+  })
+});
+
+ app.use('/api/historical-rates', function(req, res, next) {
+   console.log(req.headers);
+   console.log(req.headers.authorization);
+   console.log(req.query);
+   historicalRatesProxy.web(req, res, {
+      target: baseUrl + 'ext-services/bank-statistics',
+      auth: 'login:pass'
+  }, next); 
+});
+ 
 
 app.use(express.static(clientPath));
 
