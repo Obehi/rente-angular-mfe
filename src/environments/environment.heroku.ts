fffa0404c18baac82a8b458717bad07f11238cc9
Obehi <stian.obehi@gmail.com>
2020-09-30 14:17:55 +0200

    Fix spelling errors and change text in counter

--- src/environments/environment.heroku.ts
+++ src/environments/environment.heroku.ts
@@ -1,7 +1,7 @@
+
 export const environment = {
-    name: 'locale',
-    production: true,
-    baseUrl: 'https://rente-gateway-dev.herokuapp.com',
-    crawlerUrl: 'https://rente-ws-dev.herokuapp.com/ws'
-  };
-  
+  name: 'undefined',
+  production: false,
+  baseUrl: 'undefined',
+  crawlerUrl: 'undefined'
+};
