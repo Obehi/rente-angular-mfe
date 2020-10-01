40d9b4181c875da3a3b4b73a7ddf67c487b8de77
Obehi <stian.obehi@gmail.com>
2020-10-01 21:45:08 +0200

    Fix move-locale

--- post-install.js
+++ post-install.js
@@ -2,22 +2,23 @@ const { exec } = require('child_process');
 
 let command;
 
-var locale = ""
+var locale = ",sv"
 if (process.env.LOCALE == "sv")
     locale = ",sv"
 else if (process.env.LOCALE == "nb")
     locale = ",nb"
 
+console.log("locale is: " + locale);
 
 if (process.env.ENV === 'dev') {
     console.log("running heroku dev")
-    command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-dev' + locale );
+    command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-dev' + locale +  '&& node move-locale-dir.js');
 } else if (process.env.ENV === 'prod') {
     console.log("running heroku prod")
-    command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-prod'  + locale );
+    command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-prod'  + locale +  '&& node move-locale-dir.js');
 } else {
     console.log("cant find env. running heroku dev")
-    command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-dev' + locale);
+    command = exec('npm run config-env && npm run i18n-poeditor && ng build --configuration=heroku-dev' + locale +  '&& node move-locale-dir.js');
 }
 
 console.log("locale enviornment is:" + process.env.LOCALE)
