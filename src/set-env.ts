e8c990ca83d59cf7219f2def40ecbbd81a54bf6f
Obehi <stian.obehi@gmail.com>
2020-10-22 12:18:03 +0200

    Update set-env.ts to include tinkUrl

--- src/set-env.ts
+++ src/set-env.ts
@@ -6,12 +6,21 @@ const targetPath = path.join(__dirname + `/environments/environment.heroku.ts`);
 
 // Setup Heroku configs
 const envConfigFile = `
-export const environment = {
-  name: '${environment || "locale"}',
+export const environment: Environment = {
+  name: '${environment}',
   production: ${isProd},
   baseUrl: '${process.env.BASE_URL}',
-  crawlerUrl: '${process.env.CRAWLER_URL}'
+  crawlerUrl: '${process.env.CRAWLER_URL}',
+  tinkUrl: '${process.env.TINK_LINK}'
 };
+
+interface Environment { 
+  name: string | null,
+  production: boolean | null, 
+  baseUrl: string | null,
+  crawlerUrl: string | null,
+  tinkUrl: string | null
+}
 `;
 
 fs.writeFile(targetPath, envConfigFile, (err) => {
