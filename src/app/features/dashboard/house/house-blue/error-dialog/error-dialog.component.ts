9a4dde00502e368be8fb3f02faedc62875ee71cb
Obehi <stian.obehi@gmail.com>
2020-10-30 18:35:53 +0100

    Complete manual merge

---
+++ src/app/features/dashboard/house/house-blue/error-dialog/error-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { OfferInfo } from "@shared/models/offers";
+
+@Component({
+  selector: "rente-dialog-info",
+  templateUrl: "./error-dialog.component.html",
+  styleUrls: ["./error-dialog.component.scss"]
+})
+export class HouseFormErrorDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<HouseFormErrorDialogComponent>,
+
+  ) {}
+
+  public onClose(): void {
+    this.dialogRef.close();
+  }
+}
