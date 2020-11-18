77c42b5c3d17e217cccd0688096e4194d8bdf8f5
Obehi <stian.obehi@gmail.com>
2020-11-18 15:30:33 +0100

    added checks for social media

--- src/app/features/landing/landing-top-sv/landing-top-sv.component.ts
+++ src/app/features/landing/landing-top-sv/landing-top-sv.component.ts
@@ -9,13 +9,26 @@ import { ROUTES_MAP } from '@config/routes-config';
 export class LandingTopSvComponent implements OnInit {
   public routesMap = ROUTES_MAP
   get isMobile(): boolean { return window.innerWidth < 600; }
-  
+  isSnapchat: string
+  isFacebook: string
+  isInstagram: string
+
   agentTest: string;
 
   constructor() { }
   
   ngOnInit(): void {
-    this.agentTest = window.navigator.userAgent
+    this.agentTest = window.navigator.userAgent;
+    var regex = new RegExp("snapchat"); 
+    this.agentTest = /snapchat/i.test("somethingsnapchatsomething") ? "is snapchat" : "is not snapchat"
+
+    
+    this.isSnapchat = /snapchat/i.test(window.navigator.userAgent) ? "is snapchat" : "is not snapchat"
+    this.isFacebook = (/FBAN/i.test(window.navigator.userAgent)|| /FBAN/i.test(window.navigator.userAgent)) ? "is facebook" : "is not facebook"
+    this.isInstagram = /Instagram/i.test(window.navigator.userAgent) ? "is insta" : "is not insta"
+
+    
+    const isIEOrEdge = /msie\s|trident\/|edge\//i.test(window.navigator.userAgent)
 
   }
 
