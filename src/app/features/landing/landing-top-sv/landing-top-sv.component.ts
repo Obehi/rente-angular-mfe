4e8a12e9b47e041291ebcc99c4c7106af7d85657
Obehi <stian.obehi@gmail.com>
2020-11-18 16:16:40 +0100

    Test chrome redirect

--- src/app/features/landing/landing-top-sv/landing-top-sv.component.ts
+++ src/app/features/landing/landing-top-sv/landing-top-sv.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ROUTES_MAP } from '@config/routes-config';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'rente-landing-top',
@@ -12,13 +13,42 @@ export class LandingTopSvComponent implements OnInit {
   isSnapchat: string
   isFacebook: string
   isInstagram: string
+  isIos: boolean
+  isAndroid: boolean
 
   agentTest: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
   
   ngOnInit(): void {
+
+    window.open(
+      'googlechromes://ranteradar.se/' + ROUTES_MAP.bankSelect
+    );
+
     this.agentTest = window.navigator.userAgent;
+
+    this.isIos = !window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent); 
+    if(this.isIos) {
+      if(this.isCustomInAppBrowser()) {
+        // show dialog
+        return
+      }
+    }
+    
+    this.isAndroid = /Android/.test(navigator.userAgent); 
+
+    if(this.isAndroid) {
+      if(this.isCustomInAppBrowser()) {
+
+        this.router.navigate(['googlechromes://ranteradar.se/' + ROUTES_MAP.bankSelect]);
+        return
+      }
+    }
+
+
+
+
     var regex = new RegExp("snapchat"); 
     this.agentTest = /snapchat/i.test("somethingsnapchatsomething") ? "is snapchat" : "is not snapchat"
 
@@ -28,9 +58,17 @@ export class LandingTopSvComponent implements OnInit {
     this.isInstagram = /Instagram/i.test(window.navigator.userAgent) ? "is insta" : "is not insta"
 
     
-    const isIEOrEdge = /msie\s|trident\/|edge\//i.test(window.navigator.userAgent)
 
   }
 
+  isCustomInAppBrowser() {
+    let isSnapchat = /snapchat/i.test(window.navigator.userAgent) 
+    let isFacebook = (/FBAN/i.test(window.navigator.userAgent)|| /FBAN/i.test(window.navigator.userAgent))
+    let isInstagram = /Instagram/i.test(window.navigator.userAgent)
+
+    return isSnapchat || isFacebook || isInstagram;
+  }
 
 }
+
+
