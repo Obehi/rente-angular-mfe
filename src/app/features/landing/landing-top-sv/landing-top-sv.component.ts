13bd59dd256d688fc47040d747ba2eeb2d00e8a7
Obehi <stian.obehi@gmail.com>
2020-11-24 17:53:04 +0100

    .

--- src/app/features/landing/landing-top-sv/landing-top-sv.component.ts
+++ src/app/features/landing/landing-top-sv/landing-top-sv.component.ts
@@ -23,11 +23,11 @@ export class LandingTopSvComponent implements OnInit {
   constructor(private router: Router, private dialog: MatDialog) { }
   
   ngOnInit(): void {
-    this.dialog.open(ChangeBrowserDialogInfoComponent, {
+    /* this.dialog.open(ChangeBrowserDialogInfoComponent, {
       panelClass: 'custom-modalbox',
       data: { type: "button-middle"}
       });
-    return
+    return */
 
     this.agentTest = window.navigator.userAgent;
 
