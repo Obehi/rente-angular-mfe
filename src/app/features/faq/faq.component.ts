c1d3b8d122d817ea1863a5f45f4ae2d3672fa0f9
Obehi <stian.obehi@gmail.com>
2020-11-25 11:05:48 +0100

    Update swedish faq

--- src/app/features/faq/faq.component.ts
+++ src/app/features/faq/faq.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ROUTES_MAP } from '@config/routes-config';
+import { locale } from '../../config/locale/locale';
 
 @Component({
   selector: 'rente-faq',
@@ -9,10 +10,16 @@ import { ROUTES_MAP } from '@config/routes-config';
 export class FaqComponent implements OnInit {
   @Input() isFrontPage: boolean;
   public routes = ROUTES_MAP
+  public isSweden = false;
 
   constructor() { }
   
   ngOnInit() {
+    if(locale.includes("sv")) {
+      this.isSweden = true
+    } else{
+      this.isSweden = false
+    }
   }
 
 }
