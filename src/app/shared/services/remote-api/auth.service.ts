217a2b1410b354eed269b9eeb6ca9641d18bd46d
Obehi <stian.obehi@gmail.com>
2020-11-30 16:22:11 +0100

    .

--- src/app/shared/services/remote-api/auth.service.ts
+++ src/app/shared/services/remote-api/auth.service.ts
@@ -17,11 +17,9 @@ export class AuthService {
     private router: Router
   ) { }
 
-  public loginWithToken(phone: string, token: string, country: string) {
+  public loginWithToken(token: StringConstructor) {
     const url = `${API_URL_MAP.auth.base}${API_URL_MAP.auth.token}`;
     const data = {
-      identifier: phone.toString(),
-      country : country,
       token
     };
     return this.http.post(url, data)
