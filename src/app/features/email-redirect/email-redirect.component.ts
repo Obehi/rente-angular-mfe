3543af61cb4c0171cdb5f3ada2fcd9553c6b2a4c
Obehi <stian.obehi@gmail.com>
2020-12-01 11:27:51 +0100

    Fix redirect design

--- src/app/features/email-redirect/email-redirect.component.ts
+++ src/app/features/email-redirect/email-redirect.component.ts
@@ -15,33 +15,18 @@ export class EmailRedirectComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
     if(this.isMobile) {
       var element = document.getElementById("mobil-fb")
-      //var element = document.createElement("a")
       element.setAttribute("href", "fb-messenger://share?link=https://renteradar.no/&&app_id=326133992135942")
-     // element.dispatchEvent(new Event('click'))
       this.triggerEvent(element, 'click')
     } else {
       window.open(
         "https://www.facebook.com/dialog/send?app_id=326133992135942&link=https%3A%2F%2Frente-frontend-dev.herokuapp.com%2F%3Fgrsf%3Dmidspm&redirect_uri=https%3A%2F%2Frente-frontend-dev.herokuapp.com%2F"
-      );
+      ); 
     }
-   // window.open("fb-messenger://share?link=https://renteradar.no/&&app_id=326133992135942");
-  
-    
-
-  /*   if(this.) {
-      window.open("fb-messenger://share?link=https://renteradar.no/&&app_id=326133992135942");
-    } else 
-      window.open(
-        "https://www.facebook.com/dialog/send?app_id=326133992135942&link=https%3A%2F%2Frente-frontend-dev.herokuapp.com%2F%3Fgrsf%3Dmidspm&redirect_uri=https%3A%2F%2Frente-frontend-dev.herokuapp.com%2F"
-      );
-      */
     }
 
   triggerEvent( elem, event ) {
-    console.log("element dispatched!")
     var clickEvent = new Event( event ); 
     elem.dispatchEvent( clickEvent );    
   }
