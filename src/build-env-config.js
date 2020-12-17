9193edf244b4dca51e03bf41c33711d24afc3d00
Obehi <stian.obehi@gmail.com>
2020-12-17 10:46:15 +0100

    Fix env-config output path

--- src/build-env-config.js
+++ src/build-env-config.js
@@ -3,16 +3,21 @@ var fs = require('fs')
 const environment = process.env.ENV;
 const isProd = environment === 'prod';
 
-const envConfig = `window.ENV_CONFIG = {
+
+module.exports = (clientPath) => {
+
+  const envConfig = `window.ENV_CONFIG = {
     name: '${environment}',
     production: ${isProd},
     baseUrl: '${process.env.BASE_URL || 'https://rente-gateway-dev.herokuapp.com'}',
     crawlerUrl: '${process.env.CRAWLER_URL || 'https://rente-ws-dev.herokuapp.com/ws'}',
-    tinkUrl: '${process.env.TINK_LINK}'
+    tinkUrl: '${process.env.TINK_LINK}',
+    locale: '${process.env.LOCALE}',
+    test: '${process.env.LOCALE}'
 };
 `
-module.exports = (clientPath) => {
-  fs.writeFile(`./env-config.js`, envConfig, 'utf8', function (err) {
+
+  fs.writeFile(`../dist/rente-front-end/env-config.js`, envConfig, 'utf8', function (err) {
      if (err) return console.log(err);
   });
   console.log(`Environment config generated. Environment mode is ${process.env.APP} `);
