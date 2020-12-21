97ed7957f5a2bcfe3cce83116d13aacd6cb3ce54
Obehi <stian.obehi@gmail.com>
2020-12-21 17:19:14 +0100

    I give up

--- src/app/shared/services/env.service.ts
+++ src/app/shared/services/env.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@angular/core';
 //import { environment } from '../../../../dist/rente-front-end/env-config'
 import { environment } from '../../../../env/env.js'
-
-
+import { test } from '../../../../env/env-test.js'
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnvService {
 
+
   window: Window
   constructor( window: Window) { 
     this.window = window
     console.log(environment)
+
+    console.log(environment)
+
+    
+    
+    System.import('../../../../env/env-test.js').then(response => {
+      console.log("test")
+      response.environment.test().then(tast => console.log(tast))
+      console.log("envfromsystemimport")
+      console.log(response.environment)})
+    System.import('../../../../env/env.js').then(response => console.log(response.environment))
+    System.import('../../../../env/env.js').then(response => console.log(response.environment))
+
+    import('../../../../env/env.js').then(environment => {
+     console.log(environment.environment)
+    });
 
   }
 
