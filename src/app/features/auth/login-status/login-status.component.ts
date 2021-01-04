4ff3585ba2a77b498760ea3fb8d55f55594034f8
Obehi <stian.obehi@gmail.com>
2021-01-04 22:14:50 +0100

    logg env

--- src/app/features/auth/login-status/login-status.component.ts
+++ src/app/features/auth/login-status/login-status.component.ts
@@ -102,6 +102,8 @@ export class LoginStatusComponent implements OnInit, OnDestroy {
 
   initiateTinkBank() {
     console.log("initiateTinkBank")
+    console.log("this.envService.environment")
+    console.log(this.envService.environment)
     let tinkUrl = this.envService.environment.tinkUrl || "https://link.tink.com/1.0/authorize/?client_id=3973e78ee8c140edbf36e53d50132ba1&redirect_uri=https%3A%2F%2Franteradar.se&scope=accounts:read,identity:read&market=SE&locale=sv_SE&iframe=true"
     this.tinkUrl = this.sanitizer.bypassSecurityTrustResourceUrl(tinkUrl)
     this.isTinkBank = true
