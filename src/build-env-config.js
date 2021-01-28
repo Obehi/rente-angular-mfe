e4ab8f30d86d2fae2e79edd6553a9ac7999e52b3
Obehi <stian.obehi@gmail.com>
2021-01-28 19:09:32 +0100

    Fix build-env file bug

--- src/build-env-config.js
+++ src/build-env-config.js
@@ -33,18 +33,18 @@ module.exports = (localeForLocalDev) => {
     "production": "${isProd}",
     "baseUrl": "${process.env.BASE_URL}",
     "crawlerUrl": "${process.env.CRAWLER_URL}",
-    "locale": "${process.env.LOCALE}"
-    "shouldLog": "${process.env.SHOULD_LOG}"
+    "locale": "${process.env.LOCALE}",
+    "shouldLog": "${process.env.SHOULD_LOG}",
     "tinkUrl": "${process.env.TINK_LINK}",
-    "tinkNorDanskebankLink": "${process.env.TINK_NOR_DANSKEBANK_LINK}"
+    "tinkNorDanskebankLink": "${process.env.TINK_NOR_DANSKEBANK_LINK}",
     "tinkNorHandelsbankenLink": "${process.env.TINK_NOR_HANDELSBANKEN_LINK}",
-    "coralogixApiUrl": '${process.env.CORALOGIX_API_URL}',
-    "coralogixPrivateKey": '${process.env.CORALOGIX_PRIVATE_KEY}',
-    "coralogixApplicationName": '${process.env.CORALOGIX_APPLICATION_NAME}'
+    "coralogixApiUrl": "${process.env.CORALOGIX_API_URL}",
+    "coralogixPrivateKey": "${process.env.CORALOGIX_PRIVATE_KEY}",
+    "coralogixApplicationName": "${process.env.CORALOGIX_APPLICATION_NAME}"
   }
   `;
 
-  let outputFile = isLocal ? localEnvConfig : envConfig;
+  let outputFile = envConfig;
   let outputPath = '../dist/rente-front-end/assets/env-config.json';
 
   createDirectories(outputPath, () => {
@@ -64,6 +64,7 @@ function createDirectories(pathname, callback) {
     if (e) {
       console.error(e);
     } else {
+      console.log('created env file at ' + pathname);
       callback();
     }
   });
