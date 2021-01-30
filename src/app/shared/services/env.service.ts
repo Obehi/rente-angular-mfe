0a6a0737cc56aec099d90c971a6de8d2c827044c
Obehi <stian.obehi@gmail.com>
2021-01-30 17:43:13 +0100

    Add tink bank env variable on/off

--- src/app/shared/services/env.service.ts
+++ src/app/shared/services/env.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { map, tap, catchError } from 'rxjs/operators';
 import { Observable, throwError, EMPTY } from 'rxjs';
-
+import { BankVo } from '../../shared/models/bank';
 import { HttpResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 interface Environment {
   name: string | null;
@@ -16,7 +16,9 @@ interface Environment {
   coralogixApiUrl: string | null;
   coralogixPrivateKey: string | null;
   coralogixApplicationName: string | null;
-  dnbLogin: boolean;
+  loginDnbIsOn: boolean;
+  loginHandelsbankenIsOn: boolean;
+  loginDanskeIsOn: boolean;
 }
 
 import { HttpClient } from '@angular/common/http';
@@ -33,7 +35,6 @@ export class EnvService {
     baseUrl: 'https://rente-gateway-prod.herokuapp.com',
     crawlerUrl: 'https://rente-ws-prod.herokuapp.com/ws',
     shouldLog: false,
-    dnbLogin: true,
     tinkUrl:
       'https://link.tink.com/1.0/authorize/?client_id=3973e78ee8c140edbf36e53d50132ba1&redirect_uri=https%3A%2F%2Franteradar.se&scope=accounts:read,identity:read&market=SE&locale=sv_SE&iframe=true',
     locale: 'nb',
@@ -43,7 +44,10 @@ export class EnvService {
       'https://link.tink.com/1.0/authorize/credentials/no-handelsbanken-bankid?client_id=690cbe68c3df412082d5ad8a5a2335d8&redirect_uri=https%3A%2F%2Frenteradar.no&scope=accounts:read,credentials:read&market=NO&locale=no_NO&iframe=true',
     coralogixApiUrl: 'https://api.coralogix.com/api/v1/logs',
     coralogixPrivateKey: '92caa3a2-90d2-9f01-7d00-077afb69d8e5',
-    coralogixApplicationName: 'rente-frontend-prod_13639'
+    coralogixApplicationName: 'rente-frontend-prod_13639',
+    loginHandelsbankenIsOn: false,
+    loginDanskeIsOn: true,
+    loginDnbIsOn: true
   };
 
   private tinkBanks = {
@@ -68,7 +72,7 @@ export class EnvService {
       .toPromise();
   }
 
-  getTinkLinkForBank(bankName: any): string {
+  getTinkLinkForBank(bankName: string): string {
     return this.tinkBanks[bankName];
   }
 
@@ -78,6 +82,15 @@ export class EnvService {
 
   isNorway(): boolean {
     return this.environment.locale === 'nb' ? true : false;
+  }
+
+  isMissing(bank: BankVo): boolean {
+    return (
+      (bank.name === 'DANSKE_BANK' &&
+        this.environment.loginDanskeIsOn === false) ||
+      (bank.name === 'HANDELSBANKEN' &&
+        this.environment.loginHandelsbankenIsOn === false)
+    );
   }
 
   handleEnvFile(returnedEnv: Environment): void {
