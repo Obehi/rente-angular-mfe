e64bb893fc4af5680700131186ab6efb1dc36205
Obehi <stian.obehi@gmail.com>
2021-02-09 15:30:09 +0100

    Fixed typo

--- src/set-env.ts
+++ src/set-env.ts
@@ -5,31 +5,32 @@ const isProd = environment === 'prod';
 const targetPath = path.join(__dirname + `/environments/environment.heroku.ts`);
 
 // Setup Heroku configs
-const envConfigFile = `
-export const environment: Environment = {
+const envConfigFile = `export const environment: Environment = {
   name: '${environment}',
   production: ${isProd},
   baseUrl: '${process.env.BASE_URL}',
   crawlerUrl: '${process.env.CRAWLER_URL}',
-  'locale": '${process.env.LOCALE}',
+  locale: '${process.env.LOCALE}',
   tinkUrl: '${process.env.TINK_LINK}',
-  "tinkNorDanskebankLink": "${process.env.TINK_NOR_DANSKEBANK_LINK}",
-  "tinkNorHandelsbankenLink": "${process.env.TINK_NOR_HANDELSBANKEN_LINK}",
+  tinkNorDanskebankLink: '${process.env.TINK_NOR_DANSKEBANK_LINK}',
+  tinkNorHandelsbankenLink: '${process.env.TINK_NOR_HANDELSBANKEN_LINK}',
   coralogixApiUrl: '${process.env.CORALOGIX_API_URL}',
   coralogixPrivateKey: '${process.env.CORALOGIX_PRIVATE_KEY}',
   coralogixApplicationName: '${process.env.CORALOGIX_APPLICATION_NAME}'
-  
 };
 
-interface Environment { 
-  name: string | null,
-  production: boolean | null, 
-  baseUrl: string | null,
-  crawlerUrl: string | null,
-  tinkUrl: string | null,
-  coralogixApiUrl: string | null,
-  coralogixPrivateKey: string | null,
-  coralogixApplicationName: string | null
+interface Environment {
+  name: string | null;
+  production: boolean | null;
+  baseUrl: string | null;
+  crawlerUrl: string | null;
+  tinkUrl: string | null;
+  locale: string | null;
+  tinkNorDanskebankLink: string | null;
+  tinkNorHandelsbankenLink: string | null;
+  coralogixApiUrl: string | null;
+  coralogixPrivateKey: string | null;
+  coralogixApplicationName: string | null;
 }
 `;
 
