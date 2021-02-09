e038b6c902b7b8450c188bc9bc577b497db52292
Obehi <stian.obehi@gmail.com>
2021-02-09 15:42:15 +0100

    Fixed typo

--- src/build-environment.js
+++ src/build-environment.js
@@ -54,7 +54,7 @@ module.exports = (localeForLocalDev) => {
     "VAR_1": ${process.env.SHOULD_LOG},
     "VAR_2": ${process.env.LOGIN_DNB_IS_ON},
     "VAR_3": ${process.env.LOGIN_HANDELSBANKEN_IS_ON},
-    "VAR_4": ${process.env.LOGIN_DANSKE_IS_ON},
+    "VAR_4": ${process.env.LOGIN_DANSKE_IS_ON}
   }
   `;
 
