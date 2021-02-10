47ab4279c39d926ed7e44722d385f64ad47394f4
Obehi <stian.obehi@gmail.com>
2021-02-10 09:07:02 +0100

    Env assignment change

--- src/app/shared/services/env.service.ts
+++ src/app/shared/services/env.service.ts
@@ -102,10 +102,10 @@ export class EnvService {
     console.log('returnedEnv');
     console.log(returnedEnv);
 
-    this.environment['shouldLog'] = returnedEnv['VAR_1'];
-    this.environment['loginDnbIsOn'] = returnedEnv['VAR_2'];
-    this.environment['loginHandelsbankenIsOn'] = returnedEnv['VAR_3'];
-    this.environment['loginDanskeIsOn'] = returnedEnv['VAR_4'];
+    this.environment.shouldLog = returnedEnv['VAR_1'];
+    this.environment.loginDnbIsOn = returnedEnv['VAR_2'];
+    this.environment.loginHandelsbankenIsOn = returnedEnv['VAR_3'];
+    this.environment.loginDanskeIsOn = returnedEnv['VAR_4'];
 
     console.log('env extended');
     console.log(this.environment);
