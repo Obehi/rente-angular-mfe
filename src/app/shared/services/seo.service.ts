d57d5198cab3555bb0e26511c3935f191a8d0db4
Obehi <stian.obehi@gmail.com>
2021-02-11 17:06:50 +0100

    Ensure only one canonical link

--- src/app/shared/services/seo.service.ts
+++ src/app/shared/services/seo.service.ts
@@ -8,9 +8,24 @@ export class SeoService {
   constructor(@Inject(DOCUMENT) private doc) {}
 
   createLinkForCanonicalURL() {
+
+    var links = Array.from(this.doc.head.getElementsByTagName('link'));
+
+    // remove previous canonical link elements
+    links.filter((link) => {
+     return link.getAttribute('rel') === 'canonical' 
+    }).forEach( (link) => {
+      var toBeRemoved = link as HTMLElement
+      toBeRemoved.remove()
+    })
+
+   
     const link: HTMLLinkElement = this.doc.createElement('link');
     link.setAttribute('rel', 'canonical');
     this.doc.head.appendChild(link);
     link.setAttribute('href', this.doc.URL);
+    var linkars = Array.from(this.doc.head.getElementsByTagName('link'));
+    console.log("linkars")
+    console.log(linkars)
   }
 }
