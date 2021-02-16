cf15e099fd3e481c36438fd5718e931b828e96ca
Obehi <stian.obehi@gmail.com>
2021-02-16 12:12:56 +0100

    Add some field

---
+++ src/app/features/get-notified/get-notified-sv/get-notified-sv.component.ts
@@ -0,0 +1,136 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BankVo } from '../../../shared/models/bank';
+import { ROUTES_MAP } from '@config/routes-config';
+import { MatAutocomplete } from '@angular/material';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  NgForm,
+  FormControl
+} from '@angular/forms';
+import { VALIDATION_PATTERN } from '@config/validation-patterns.config';
+import { Observable, timer, EMPTY } from 'rxjs';
+import { ENTER, COMMA } from '@angular/cdk/keycodes';
+import { debounce } from 'rxjs/operators';
+import { ContactService } from '../../../shared/services/remote-api/contact.service';
+import { Router } from '@angular/router';
+import { SnackBarService } from '@services/snackbar.service';
+
+@Component({
+  selector: 'rente-get-notified',
+  templateUrl: './get-notified-sv.component.html',
+  styleUrls: ['./get-notified-sv.component.scss']
+})
+export class GetNotifiedSvComponent implements OnInit {
+  public missingBankForm: FormGroup;
+  public visible = true;
+  public selectable = true;
+  public removable = true;
+  public addOnBlur = true;
+  public separatorKeysCodes: number[] = [ENTER, COMMA];
+  // public bankCtrl = new FormControl();
+  public filteredBanks: Observable<string[]>;
+  public banks: any = [];
+  public allBanks: any[];
+  public isLoading: boolean;
+  public emailError = false;
+
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
+  constructor(
+    private fb: FormBuilder,
+    private contactService: ContactService,
+    private router: Router,
+    private snackBar: SnackBarService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.missingBankForm = this.fb.group({
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(VALIDATION_PATTERN.email)
+        ])
+      ],
+      bank: ['', Validators.compose([Validators.required])]
+    });
+    this.missingBankForm
+      .get('email')
+      .valueChanges.pipe(
+        debounce((data) => {
+          this.emailError = false;
+          return this.inValid() ? timer(2000) : EMPTY;
+        })
+      )
+      .subscribe((data) => (this.emailError = this.inValid()));
+  }
+
+  inValid() {
+    return (
+      this.missingBankForm.get('email').hasError('pattern') &&
+      this.missingBankForm.get('email').dirty
+    );
+  }
+
+  onBlurErrorCheck() {
+    this.emailError = this.inValid();
+  }
+
+  // TODO: Move to service
+  public isErrorState(
+    control: AbstractControl | null,
+    form: FormGroup | NgForm | null
+  ): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+
+  public request() {
+    this.isLoading = true;
+
+    const missingBankData = {
+      email: this.missingBankForm.value.email,
+      bank: this.missingBankForm.value.bank
+    };
+
+    this.contactService.sendMissingBank(missingBankData).subscribe(
+      (_) => {
+        this.isLoading = false;
+        this.router.navigate(['/']);
+        this.snackBar.openSuccessSnackBar(
+          'Du får beskjed når din bank er tilgjengelig',
+          1.2
+        );
+      },
+      (err) => {
+        this.isLoading = false;
+      }
+    );
+  }
+
+  public noWhitespaceValidator(control: FormControl) {
+    if (control == null) return null;
+
+    const isWhitespace =
+      (control.value.name || control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { whitespace: true };
+  }
+
+  public displayFn(bank: any): string | undefined {
+    return bank ? bank.name : undefined;
+  }
+
+  private filter(value: any): any[] {
+    const filterValue = value.name
+      ? value.name.toLowerCase()
+      : value.toLowerCase();
+    return this.allBanks.filter((bank) =>
+      bank.name.toLowerCase().includes(filterValue)
+    );
+  }
+}
--- src/environments/environment.heroku.ts
+++ src/environments/environment.heroku.ts
@@ -1,27 +1,16 @@
-export const environment: Environment = {
-  name: 'undefined',
+export const environment = {
+  name: 'local',
   production: false,
-  baseUrl: 'undefined',
-  crawlerUrl: 'undefined',
-  locale: 'undefined',
-  tinkUrl: 'undefined',
-  tinkNorDanskebankLink: 'undefined',
-  tinkNorHandelsbankenLink: 'undefined',
-  coralogixApiUrl: 'undefined',
-  coralogixPrivateKey: 'undefined',
-  coralogixApplicationName: 'undefined'
+  baseUrl: 'https://rente-gateway-dev.herokuapp.com',
+  crawlerUrl: 'https://rente-ws-dev.herokuapp.com/ws',
+  tinkUrl:
+    'https://link.tink.com/1.0/authorize/?client_id=3973e78ee8c140edbf36e53d50132ba1&redirect_uri=https%3A%2F%2Franteradar.se&scope=accounts:read,identity:read&market=SE&locale=sv_SE&iframe=true',
+  locale: 'sv',
+  tinkNorDanskebankLink:
+    'https://link.tink.com/1.0/authorize/credentials/no-danskebank-password?client_id=690cbe68c3df412082d5ad8a5a2335d8&redirect_uri=https%3A%2F%2Frenteradar.no&scope=accounts:read,credentials:read&market=NO&locale=no_NO&iframe=true',
+  tinkNorHandelsbankenLink:
+    'https://link.tink.com/1.0/authorize/credentials/no-handelsbanken-bankid?client_id=690cbe68c3df412082d5ad8a5a2335d8&redirect_uri=https%3A%2F%2Frenteradar.no&scope=accounts:read,credentials:read&market=NO&locale=no_NO&iframe=true',
+  coralogixApiUrl: 'https://api.coralogix.com/api/v1/logs',
+  coralogixPrivateKey: '92caa3a2-90d2-9f01-7d00-077afb69d8e5',
+  coralogixApplicationName: 'rente-frontend-prod_13639'
 };
-
-interface Environment {
-  name: string | null;
-  production: boolean | null;
-  baseUrl: string | null;
-  crawlerUrl: string | null;
-  tinkUrl: string | null;
-  locale: string | null;
-  tinkNorDanskebankLink: string | null;
-  tinkNorHandelsbankenLink: string | null;
-  coralogixApiUrl: string | null;
-  coralogixPrivateKey: string | null;
-  coralogixApplicationName: string | null;
-}
