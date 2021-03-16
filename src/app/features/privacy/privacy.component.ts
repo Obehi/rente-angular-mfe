82f1f36b566458567bfeadd026e80507b2e5b260
Obehi <stian.obehi@gmail.com>
2021-03-16 10:51:11 +0100

    Fix privacy bug

--- src/app/features/privacy/privacy.component.ts
+++ src/app/features/privacy/privacy.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SeoService } from '@services/seo.service';
+import { ROUTES_MAP } from '@config/routes-config';
+import { EnvService } from '@services/env.service';
 
 @Component({
   selector: 'rente-privacy',
@@ -7,9 +9,19 @@ import { SeoService } from '@services/seo.service';
   styleUrls: ['./privacy.component.scss']
 })
 export class PrivacyComponent implements OnInit {
-  constructor(private seoService: SeoService) {}
-
+  constructor(private seoService: SeoService, private envService: EnvService) {}
+  privacyPolicyLink: string;
   ngOnInit(): void {
+    console.log('this.envService.isNorway');
+    console.log(this.envService.isNorway());
+    console.log(this.envService.isSweden());
+    if (this.envService.isNorway) {
+      this.privacyPolicyLink = 'personvernerklaering';
+    }
+
+    if (this.envService.isSweden) {
+      this.privacyPolicyLink = 'integritypolicy';
+    }
     this.seoService.createLinkForCanonicalURL();
   }
 }
