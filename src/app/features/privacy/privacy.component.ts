f8c1c7b7cce34fa43d658a20d4f10774293bfa05
Obehi <stian.obehi@gmail.com>
2021-03-16 11:30:20 +0100

    Remove logs

--- src/app/features/privacy/privacy.component.ts
+++ src/app/features/privacy/privacy.component.ts
@@ -12,9 +12,6 @@ export class PrivacyComponent implements OnInit {
   constructor(private seoService: SeoService, private envService: EnvService) {}
   privacyPolicyLink: string;
   ngOnInit(): void {
-    console.log('this.envService.isNorway');
-    console.log(this.envService.isNorway());
-    console.log(this.envService.isSweden());
     if (this.envService.isNorway()) {
       this.privacyPolicyLink = 'personvernerklaering';
     }
