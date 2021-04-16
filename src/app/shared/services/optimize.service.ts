0618313a6feef7bc744b261aad3fb42f3a971d67
Obehi <stian.obehi@gmail.com>
2021-04-16 18:11:07 +0200

    change optimize experimentId

--- src/app/shared/services/optimize.service.ts
+++ src/app/shared/services/optimize.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OptimizeService {
-  private experimentID = '486O0RUlSMCPtcnEjUtFtg';
+  private experimentID = 'xV_v0ABdSQuZIsJEHiDgow';
   private localeVariation = 0;
 
   getVariation(): number {
