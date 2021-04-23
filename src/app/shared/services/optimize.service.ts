cd5741509f3afdbf83c492fb491c6fee8184b2ce
Obehi <stian.obehi@gmail.com>
2021-04-23 16:01:21 +0200

    Remove logs

--- src/app/shared/services/optimize.service.ts
+++ src/app/shared/services/optimize.service.ts
@@ -21,20 +21,6 @@ export class OptimizeService {
 
     const variation = (window as any).google_optimize.get(this.experimentID);
 
-    console.log('variation');
-    console.log(variation);
-    console.log('exp variable');
-    console.log(
-      ((window as any).google_optimize as any) &&
-        (window as any).google_optimize.get(this.experimentID)
-    );
-
-    console.log('exp hardcode');
-
-    console.log(
-      ((window as any).google_optimize as any) &&
-        (window as any).google_optimize.get('9d84Epc8T3amY5DsACFhVA')
-    );
     if (variation === undefined) {
       return 0;
     }
