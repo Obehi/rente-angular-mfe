208923e2f71e7b9801ca243439eab3aa00f74d79
Obehi <stian.obehi@gmail.com>
2021-04-27 15:17:13 +0200

    add form

--- src/app/features/auth/bank-id-login/bank-id-login.component.ts
+++ src/app/features/auth/bank-id-login/bank-id-login.component.ts
@@ -1,7 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { PlatformLocation } from '@angular/common';
+import {
+  Component,
+  ViewChild,
+  OnInit,
+  HostListener,
+  ElementRef
+} from '@angular/core';
+import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
 import { LocalStorageService } from '@services/local-storage.service';
 import { AuthService } from '@services/remote-api/auth.service';
+import { Observable, of, from, fromEvent } from 'rxjs';
+import { startWith, map, tap } from 'rxjs/operators';
+import {
+  MatAutocomplete,
+  MatAutocompleteSelectedEvent
+} from '@angular/material';
+import { VALIDATION_PATTERN } from '@config/validation-patterns.config';
+import { Mask } from '@shared/constants/mask';
+import {
+  ConfirmationGetDto,
+  LoansService,
+  MembershipTypeDto
+} from '@services/remote-api/loans.service';
+import { NavigationInterceptionService } from '@services/navigation-interception.service';
+import { MatStepper } from '@angular/material';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  FormControl
+} from '@angular/forms';
+import { forkJoin } from 'rxjs';
+import { LoanUpdateInfoDto } from '@shared/models/loans';
 
 @Component({
   selector: 'rente-bank-id-login',
@@ -9,24 +40,209 @@ import { AuthService } from '@services/remote-api/auth.service';
   styleUrls: ['./bank-id-login.component.scss']
 })
 export class BankIdLoginComponent implements OnInit {
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event) {
+    console.log('Back button pressed');
+  }
+  @ViewChild('stepper') stepper: MatStepper;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+  @ViewChild('membershipInput') membershipInput: ElementRef<HTMLInputElement>;
+  eventSubscription;
+  userFormGroup: FormGroup;
+  loanFormGroup: FormGroup;
+
+  public removable = true;
+  public membershipCtrl = new FormControl();
+  public filteredMemberships: Observable<void | MembershipTypeDto[]>;
+  public memberships: any = [];
+  public allMemberships: MembershipTypeDto[];
+  public showForms = false;
+  public mask = Mask;
+  public isLoading = true;
+
   constructor(
     private route: ActivatedRoute,
     private authService: AuthService,
-    private localStorageService: LocalStorageService
-  ) {}
+    private localStorageService: LocalStorageService,
+    private router: Router,
+    location: PlatformLocation,
+    private navigationInterceptionService: NavigationInterceptionService,
+    private fb: FormBuilder,
+    private loanService: LoansService
+  ) {
+    window.onpopstate = function (event) {
+      event.preventDefault();
+      alert(
+        `location: ${document.location}, state: ${JSON.stringify(event.state)}`
+      );
+    };
+
+    // history.back(); // alerts "location: http://example.com/example.html?page=1, state: {"page":1}"
+    location.onPopState(() => {
+      alert(window.location);
+    });
+    router.events.subscribe((event: NavigationStart) => {
+      console.log('any state');
+
+      console.log(event);
+
+      if (event.navigationTrigger === 'popstate') {
+        console.log('popstate');
+        alert('popstate');
+        // Perform actions
+      }
+    });
+  }
+
   ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
-      const status = params['status'];
-      const sessionId = params['sessionId'];
+    forkJoin([
+      this.loanService.getConfirmationData(),
+      this.loanService.getOffersBanks()
+    ]).subscribe(([userInfo, offerBanks]) => {
+      this.initMemberships(userInfo);
+      this.initForms();
+      this.showForms = true;
+      this.isLoading = false;
+      console.log('offerBanks');
+      console.log(offerBanks);
+    });
+
+    this.route.queryParams.subscribe((routeParams) => {
+      const status = routeParams['status'];
+      const sessionId = routeParams['sessionId'];
 
       const bank = this.localStorageService.getItem('bankIdLoginBank');
-      console.log('bank');
-      console.log(bank);
+
       this.authService
-        .loginBankIdStep2('d7adeb5108884f4cbfe8ad1500d7f155', bank)
+        .loginBankIdStep2('1e85f172698745a99148ad1600bd5e03', bank)
         .subscribe((response) => {
           console.log(response);
         });
     });
+
+    /*  window.history.pushState({}, '', '/newpage'); */
+    /*    history.pushState('{ page: 1 }', 'title 1', '?page=1');
+    history.pushState('{ page: 2 }', 'title 2', '?page=2');
+    window.history.pushState({}, '', '/oldpage'); */
+    // history.back(); // alerts "location: http://example.com/example.html?page=1, state: {"page":1}"
+    /* 
+    
+
+    this.eventSubscription = fromEvent(window, 'popstate').subscribe((e) => {
+      console.log(e, 'back button');
+      alert('eventSubscription');
+    }); */
   }
+
+  public isErrorState(control: AbstractControl | null): boolean {
+    /*     console.log('control start: ');
+    console.log('control?.invalid');
+    console.log(control?.invalid);
+    console.log('control?.dirty');
+    console.log(control?.dirty);
+    console.log('control?.touched');
+    console.log(control?.touched); */
+
+    console.log(
+      !!(control && control.invalid && (control.dirty || control.touched))
+    );
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+
+  private filter(value: any): void {
+    const filterValue = value.label
+      ? value.label.toLowerCase()
+      : value.toLowerCase();
+    this.allMemberships = this.clearDuplicates(
+      this.allMemberships,
+      this.memberships
+    );
+  }
+
+  remove(membership, index): void {
+    this.allMemberships.push(membership);
+    this.allMemberships.sort((a, b) =>
+      a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+    );
+    this.memberships.splice(index, 1);
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.memberships.push(event.option.value);
+    this.membershipInput.nativeElement.value = '';
+    this.membershipCtrl.setValue(null);
+  }
+
+  private clearDuplicates(
+    array: MembershipTypeDto[],
+    toRemoveArray: MembershipTypeDto[]
+  ): MembershipTypeDto[] {
+    for (let i = array.length - 1; i >= 0; i--) {
+      // tslint:disable-next-line:prefer-for-of
+      for (let j = 0; j < toRemoveArray.length; j++) {
+        if (array[i] && array[i].name === toRemoveArray[j].name) {
+          array.splice(i, 1);
+        }
+      }
+    }
+    return array;
+  }
+  /* @HostListener('window:popstate', ['$event'])
+  onBrowserBackBtnClose(event: Event) {
+    console.log('back button pressed');
+    event.preventDefault();
+    this.router.navigate(['/home'], { replaceUrl: true });
+  } */
+
+  initMemberships(userInfo: ConfirmationGetDto): void {
+    this.allMemberships = userInfo.availableMemberships;
+
+    this.filteredMemberships = this.membershipCtrl.valueChanges.pipe(
+      startWith(null),
+      map((membership: string | null) =>
+        membership ? this.filter(membership) : this.allMemberships.slice()
+      )
+    );
+    this.allMemberships = userInfo.availableMemberships;
+
+    this.filteredMemberships = this.membershipCtrl.valueChanges.pipe(
+      startWith(null),
+      map((membership: string | null) =>
+        membership ? this.filter(membership) : this.allMemberships.slice()
+      )
+    );
+  }
+
+  initForms(): void {
+    this.userFormGroup = this.fb.group({
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(VALIDATION_PATTERN.email)
+        ])
+      ],
+      address: ['', Validators.required],
+      zip: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(VALIDATION_PATTERN.zip)
+        ])
+      ],
+      apartmentSize: [
+        '',
+        [Validators.required, Validators.min(5), Validators.max(999)]
+      ],
+      membership: [],
+      income: ['', Validators.required]
+    });
+
+    this.loanFormGroup = this.fb.group({
+      outstandingDebt: ['', Validators.required],
+      remainingYears: ['', Validators.required, Validators.max(100)]
+    });
+  }
+
+  update() {}
 }
