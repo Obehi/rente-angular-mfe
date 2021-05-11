2b3c6634cf3e799e0ed0709f93f0be11c3df5c20
karolispt <31269502+karolispt@users.noreply.github.com>
2021-05-11 15:46:46 +0200

    a

--- src/app/features/first-buyers/first-buyers.module.ts
+++ src/app/features/first-buyers/first-buyers.module.ts
@@ -14,7 +14,6 @@ import { FirstBuyersRoutingModule } from './first-buyers-routing.module';
 import { FirstBuyersComponent } from './first-buyers.component';
 import { FirstBuyersService } from './first-buyers.service';
 import { PropertySelectComponent } from './components/property-select/property-select.component';
-import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
   declarations: [
@@ -33,8 +32,7 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
     MaterialModule,
     ReactiveFormsModule,
     SelectAutocompleteModule,
-    CurrencyMaskModule,
-    ScrollingModule
+    CurrencyMaskModule
   ],
   providers: [
     {
