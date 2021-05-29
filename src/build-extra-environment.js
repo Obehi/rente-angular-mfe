bae1e8653bf063a98b902645ae7f4f8ba1a8b602
Obehi <stian.obehi@gmail.com>
2021-05-29 21:01:05 +0200

    Add extra variables

--- src/build-extra-environment.js
+++ src/build-extra-environment.js
@@ -33,8 +33,8 @@ module.exports = (localeForLocalDev) => {
     "VAR_2": ${process.env.LOGIN_DNB_IS_ON},
     "VAR_3": ${process.env.LOGIN_HANDELSBANKEN_IS_ON},
     "VAR_4": ${process.env.LOGIN_DANSKE_IS_ON},
-    "VAR_5": '${process.env.SB1_DISABLED_BANKS}',
-    "VAR_6": '${process.env.SB1TRY_AGAIN_DOWNTIME}'
+    "VAR_5": ${process.env.SB1_DISABLED_BANKS},
+    "VAR_6": ${process.env.SB1TRY_AGAIN_DOWNTIME}
   }
   `;
 
