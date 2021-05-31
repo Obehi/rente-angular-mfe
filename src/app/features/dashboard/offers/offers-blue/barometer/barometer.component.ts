aae3d0c7247232c15f2d7680b897ed018d2b7c6f
karolispt <31269502+karolispt@users.noreply.github.com>
2021-05-31 11:55:53 +0200

    updated algorithm for rente status

--- src/app/features/dashboard/offers/offers-blue/barometer/barometer.component.ts
+++ src/app/features/dashboard/offers/offers-blue/barometer/barometer.component.ts
@@ -11,17 +11,94 @@ export class BarometerComponent implements OnInit {
   @Input() offersInfo: Offers;
   @Input() offerSavingsType = OFFER_SAVINGS_TYPE;
 
+  baseTextArray = [
+    'Renten din er godt over gjennomsnittet',
+    'Renten din er over gjennomsnitt',
+    'Renten din er gjennomsnittnlig',
+    'Renten din er god',
+    'Renten din er svært god'
+  ];
+  additionalTextArray = [
+    'og du har mye å spare',
+    'og du har mye å spare',
+    'men du kan spare en del',
+    'men du kan spare en del',
+    'men du kan spare en del'
+  ];
+  state = 0;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.calcState();
+  }
+
+  calcState(): any {
+    const avg = this.offersInfo.bankStatistics.allBanksStatistics
+      .medianEffectiveRate;
+    const totalEffectiveRate = this.offersInfo.totalEffectiveRate;
+    const z = this.effectiveInterest;
+
+    if (totalEffectiveRate !== null) {
+      if (avg + 2 * z < totalEffectiveRate) {
+        this.state = 0;
+      }
+      if (avg + z < totalEffectiveRate && totalEffectiveRate <= avg + 2 * z) {
+        this.state = 1;
+      }
+      if (avg - z < totalEffectiveRate && totalEffectiveRate <= avg + z) {
+        this.state = 2;
+      }
+      if (avg - 2 * z < totalEffectiveRate && totalEffectiveRate <= avg - z) {
+        this.state = 3;
+      }
+      if (totalEffectiveRate <= avg - 2 * z) {
+        this.state = 4;
+      }
+    }
+  }
 
   get rateBarPercentageInverted(): number {
     return 100 - this.rateBarPercentage.percentage;
   }
 
+  get effectiveInterest(): any {
+    const x = this.offersInfo.bankStatistics.allBanksStatistics
+      .medianEffectiveRate;
+    const y = this.offersInfo.bankStatistics.allBanksStatistics
+      .bestPercentileEffectiveRate;
+
+    return (x - y) / 3;
+  }
+
+  get shouldShowAdditionalText(): boolean | undefined {
+    if (
+      this.offersInfo.offerSavingsType ===
+        'SAVINGS_FIRST_YEAR_BETWEEN_0_AND_2000' ||
+      this.offersInfo.offerSavingsType ===
+        'SAVINGS_FIRST_YEAR_BETWEEN_2000_AND_6000'
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get text(): string | undefined {
+    if (this.shouldShowAdditionalText) {
+      return (
+        this.baseTextArray[this.state] +
+        ', ' +
+        this.additionalTextArray[this.state]
+      );
+    } else {
+      return this.baseTextArray[this.state];
+    }
+  }
+
   get rateBarPercentage(): RateBar {
-    switch (this.offersInfo.offerSavingsType) {
-      case this.offerSavingsType.NO_SAVINGS: {
+    switch (this.state) {
+      case 4: {
         return {
           percentage: 95,
           class: 'level-5',
@@ -29,7 +106,7 @@ export class BarometerComponent implements OnInit {
         };
         break;
       }
-      case this.offerSavingsType.SAVINGS_FIRST_YEAR_BETWEEN_0_AND_2000: {
+      case 3: {
         return {
           percentage: 75,
           class: 'level-4',
@@ -37,7 +114,7 @@ export class BarometerComponent implements OnInit {
         };
         break;
       }
-      case this.offerSavingsType.SAVINGS_FIRST_YEAR_BETWEEN_2000_AND_6000: {
+      case 2: {
         return {
           percentage: 50,
           class: 'level-3',
@@ -45,7 +122,7 @@ export class BarometerComponent implements OnInit {
         };
         break;
       }
-      case this.offerSavingsType.SAVINGS_FIRST_YEAR_BETWEEN_6000_AND_10000: {
+      case 1: {
         return {
           percentage: 30,
           class: 'level-2',
@@ -53,7 +130,7 @@ export class BarometerComponent implements OnInit {
         };
         break;
       }
-      case this.offerSavingsType.SAVINGS_FIRST_YEAR_GREATER_10000: {
+      case 0: {
         return {
           percentage: 12,
           class: 'level-1',
