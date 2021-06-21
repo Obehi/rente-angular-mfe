8c0fceaeb4a7dee9ce1759b0f87104148c149e83
Obehi <stian.obehi@gmail.com>
2021-06-21 10:43:31 +0200

    .

--- src/app/features/bank-select-no/bank-select.component.ts
+++ src/app/features/bank-select-no/bank-select.component.ts
@@ -140,6 +140,7 @@ export class BankSelectNoComponent implements OnInit {
       bank.name === 'DNB' &&
       this.envService.environment.dnbSignicatIsOn === true
     ) {
+      bank.isSb1Bank = true;
       this.router.navigate(['/autentisering/' + ROUTES_MAP_NO.bankIdLogin], {
         state: { data: { bank: bank } }
       });
