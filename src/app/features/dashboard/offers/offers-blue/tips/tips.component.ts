cf6ad70aca44d566563a22dda9d04f4ed8a6eb82
Obehi <stian.obehi@gmail.com>
2021-07-26 10:52:52 +0200

    Fix tips sweden

--- src/app/features/dashboard/offers/offers-blue/tips/tips.component.ts
+++ src/app/features/dashboard/offers/offers-blue/tips/tips.component.ts
@@ -183,8 +183,7 @@ export class TipsComponent implements OnInit {
     ) {
       this.tips.push({
         header: 'Fastrentelån',
-        text:
-          'Vi ser du har ett eller flere fastrentelån. Renteradar viser besparelsespotensialet kun for lånet/lånene med flytende rente. Beste rente viser også kun beste rente for lånet/lånene med flytende rente.',
+        text: this.customLangTextSerice.getHasFixedRateLoan(),
         buttonLink: './',
         icon: 'rate-blue'
       });
