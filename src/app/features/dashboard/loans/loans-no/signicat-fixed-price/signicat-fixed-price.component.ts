e4ccd6ed8fb4b733c534aef6b4a7a5ec364c5bf3
maazil <matthew.ling98@gmail.com>
2021-08-18 13:25:50 +0200

    finished ui for both signicat users

--- src/app/features/dashboard/loans/loans-no/signicat-fixed-price/signicat-fixed-price.component.ts
+++ src/app/features/dashboard/loans/loans-no/signicat-fixed-price/signicat-fixed-price.component.ts
@@ -2,19 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoansService } from '@services/remote-api/loans.service';
 
+interface bankDto {
+  name: string;
+  id: string;
+  rate: number;
+}
+
 @Component({
   selector: 'rente-signicat-fixed-price',
   templateUrl: './signicat-fixed-price.component.html',
   styleUrls: ['./signicat-fixed-price.component.scss']
 })
 export class SignicatFixedPriceComponent implements OnInit {
-  public allOffers: any[];
+  public allOffers: bankDto[];
   public loansData: any;
   public loans: any[];
   public errorMessage: string;
   public isEditMode = false;
   public loanForm: FormGroup;
-  public currentLoanName = 'Domain';
+  public selected = '';
+  public loanTypeString: string;
   /*
     The object interface is not updated so fix it when the new version is merged
   */
@@ -25,15 +32,33 @@ export class SignicatFixedPriceComponent implements OnInit {
     this.loansService.getLoanAndOffersBanks().subscribe(
       ([loans, offerBank]) => {
         this.loansData = loans;
-        this.allOffers = offerBank.offers;
+        // this.allOffers = offerBank.offers;
         this.loans = this.loansData.loans;
-
-        // console.log(this.loansData.loans);
-        // console.log(this.allOffers);
 
         const dto = this.loans[0];
 
-        this.currentLoanName = String(dto.loanName);
+        // Test data
+        this.loanTypeString = 'BOLIGLÅN 85% BOLIGKR 0-2';
+        // this.loanTypeString = String(dto.loanName);
+
+        /*
+         * Create an object of the same datatype from bank offers
+         * To be able to set selected to a value
+         * Since mat-select wants the default value to be the same datatype as the array it uses
+         */
+
+        const transformDto = {
+          name: this.loanTypeString,
+          id: 'tag:finansportalen.no:feed/bank/boliglan/1622922',
+          rate: dto.nominalRate
+        };
+
+        this.allOffers = offerBank.offers;
+        this.allOffers.push(transformDto);
+
+        this.selected = this.allOffers.filter(
+          (val) => val.name === transformDto.name
+        )[0].name;
 
         this.loanForm = this.fb.group({
           loanName: [{ value: dto.loanName, disabled: true }],
@@ -46,11 +71,6 @@ export class SignicatFixedPriceComponent implements OnInit {
             Validators.required
           ]
         });
-
-        this.loanForm.get('loanName')?.setValue(this.currentLoanName);
-
-        console.log('Loan name');
-        console.log(this.loanForm.get('loanName')?.value);
       },
       (err) => {
         this.errorMessage = err.title;
