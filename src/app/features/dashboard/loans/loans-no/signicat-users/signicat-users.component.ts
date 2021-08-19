d30e55432582415e1e73153620def3ca4486c415
maazil <matthew.ling98@gmail.com>
2021-08-19 17:25:18 +0200

    added initial value for dto to store and added animation to loantype

--- src/app/features/dashboard/loans/loans-no/signicat-users/signicat-users.component.ts
+++ src/app/features/dashboard/loans/loans-no/signicat-users/signicat-users.component.ts
@@ -20,6 +20,15 @@ export class SignicatUsersComponent implements OnInit {
   public showDisplayBox = true;
   public showButton = false;
 
+  public outstandingDebtString = 'outstandingDebt';
+  public remainingYearsString = 'remainingYears';
+  public nominalRateString = 'nominalRate';
+
+  // Store the inital value from api
+  public initialOutStandingDebt: string;
+  public initialRemainingYears: string;
+  public initialNominalRate: string;
+
   /*
     The object interface is not updated so fix it when the new version is merged
   */
@@ -33,28 +42,31 @@ export class SignicatUsersComponent implements OnInit {
         this.loansData = loans;
         this.allOffers = offerBank.offers;
         this.loans = this.loansData.loans;
-        console.log(this.loansData);
 
         const dto = this.loans[0];
 
-        console.log(dto.outstandingDebt);
+        if (!dto) {
+          console.log('Dto doesnt exist!');
+        }
+
+        this.initialOutStandingDebt = String(dto.outstandingDebt);
+        this.initialRemainingYears = String(dto.remainingYears);
+        this.initialNominalRate = String(dto.nominalRate);
 
         this.loanForm = this.fb.group({
           outstandingDebt: [
-            { value: String(dto.outstandingDebt), disabled: true },
+            { value: this.initialOutStandingDebt, disabled: true },
             Validators.required
           ],
-          // outstandingDebt: new FormControl(),
           remainingYears: [
-            { value: String(dto.remainingYears), disabled: true },
+            { value: this.initialRemainingYears, disabled: true },
             Validators.required
           ],
           nominalRate: [
-            { value: String(dto.nominalRate), disabled: true },
+            { value: this.initialNominalRate, disabled: true },
             Validators.required
           ]
         });
-        console.log(this.loanForm);
       },
       (err) => {
         this.errorMessage = err.title;
@@ -62,29 +74,63 @@ export class SignicatUsersComponent implements OnInit {
     );
   }
 
-  // public activateEditMode(): void {
-  //   // this.isEditMode = !this.isEditMode;
-  //   // this.setEnabled();
-  //   this.setDisabled();
-  // }
-
   public setDisabled(): void {
     this.showButton = false;
     setTimeout(() => {
       this.isEditMode = false;
       this.showDisplayBox = true;
+
+      this.loanForm
+        .get(this.outstandingDebtString)
+        ?.setValue(this.initialOutStandingDebt);
+
+      this.loanForm
+        .get(this.remainingYearsString)
+        ?.setValue(this.initialRemainingYears);
+
+      this.loanForm
+        .get(this.nominalRateString)
+        ?.setValue(this.initialNominalRate);
     }, 325);
-    this.loanForm.get('outstandingDebt')?.disable();
-    this.loanForm.get('remainingYears')?.disable();
-    this.loanForm.get('nominalRate')?.disable();
+    this.loanForm.get(this.outstandingDebtString)?.disable();
+    this.loanForm.get(this.remainingYearsString)?.disable();
+    this.loanForm.get(this.nominalRateString)?.disable();
   }
 
   public setEnabled(): void {
     this.showDisplayBox = false;
     this.showButton = true;
     this.isEditMode = true;
-    this.loanForm.get('outstandingDebt')?.enable();
-    this.loanForm.get('remainingYears')?.enable();
-    this.loanForm.get('nominalRate')?.enable();
+    this.loanForm.get(this.outstandingDebtString)?.enable();
+    this.loanForm.get(this.remainingYearsString)?.enable();
+    this.loanForm.get(this.nominalRateString)?.enable();
+  }
+
+  public save(): void {
+    // Get the new values and then set it to initial value then send request
+    const getOutstandingDebt = this.loanForm.get(this.outstandingDebtString)
+      ?.value;
+    this.initialOutStandingDebt = getOutstandingDebt;
+    // this.loanForm
+    //   .get(this.outstandingDebtString)
+    //   ?.setValue(this.initialOutStandingDebt);
+    console.log(getOutstandingDebt);
+
+    const getRemainingYears = this.loanForm.get(this.remainingYearsString)
+      ?.value;
+    this.initialRemainingYears = getRemainingYears;
+
+    // this.loanForm
+    //   .get(this.remainingYearsString)
+    //   ?.setValue(this.initialRemainingYears);
+    console.log(getRemainingYears);
+
+    const getNominalRate = this.loanForm.get(this.nominalRateString)?.value;
+    this.initialNominalRate = getNominalRate;
+
+    // this.loanForm
+    // .get(this.remainingYearsString)
+    // ?.setValue(this.initialNominalRate);
+    console.log(getNominalRate);
   }
 }
