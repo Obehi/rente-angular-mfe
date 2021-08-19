04a6b9f54c0ce2598d2d24bbd6ce062e284c6d15
maazil <matthew.ling98@gmail.com>
2021-08-19 12:17:31 +0200

    final touches on ui crawler banks and signicat users

--- src/app/features/dashboard/loans/loans-no/signicat-users/signicat-users.component.ts
+++ src/app/features/dashboard/loans/loans-no/signicat-users/signicat-users.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoansService } from '@services/remote-api/loans.service';
+import { ButtonFadeInOut } from '@shared/animations/button-fade-in-out';
+import { FadeOut } from '@shared/animations/fade-out';
 
 @Component({
   selector: 'rente-signicat-users',
   templateUrl: './signicat-users.component.html',
-  styleUrls: ['./signicat-users.component.scss']
+  styleUrls: ['./signicat-users.component.scss'],
+  animations: [FadeOut, ButtonFadeInOut]
 })
 export class SignicatUsersComponent implements OnInit {
   public allOffers: any[];
@@ -13,10 +16,13 @@ export class SignicatUsersComponent implements OnInit {
   public loans: any[];
   public errorMessage: string;
   public isEditMode = false;
+  public loanForm: FormGroup;
+  public showDisplayBox = true;
+  public showButton = false;
+
   /*
     The object interface is not updated so fix it when the new version is merged
   */
-  public loanForm: FormGroup;
   // public setDisabled = false;
 
   constructor(private loansService: LoansService, private fb: FormBuilder) {}
@@ -63,13 +69,19 @@ export class SignicatUsersComponent implements OnInit {
   // }
 
   public setDisabled(): void {
-    this.isEditMode = false;
+    this.showButton = false;
+    setTimeout(() => {
+      this.isEditMode = false;
+      this.showDisplayBox = true;
+    }, 325);
     this.loanForm.get('outstandingDebt')?.disable();
     this.loanForm.get('remainingYears')?.disable();
     this.loanForm.get('nominalRate')?.disable();
   }
 
   public setEnabled(): void {
+    this.showDisplayBox = false;
+    this.showButton = true;
     this.isEditMode = true;
     this.loanForm.get('outstandingDebt')?.enable();
     this.loanForm.get('remainingYears')?.enable();
