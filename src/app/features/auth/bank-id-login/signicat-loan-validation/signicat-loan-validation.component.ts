83904dcccb4b5b897ea9bab8ac0bc29a5e2f43c0
maazil <matthew.ling98@gmail.com>
2021-10-22 09:19:12 +0200

    ui and loan type filter added and finished

--- src/app/features/auth/bank-id-login/signicat-loan-validation/signicat-loan-validation.component.ts
+++ src/app/features/auth/bank-id-login/signicat-loan-validation/signicat-loan-validation.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
+import { LoanTypeOption, nonListLoanType } from '@models/loan-type';
 import { LoanInfo } from '@models/loans';
 
 @Component({
@@ -11,11 +12,23 @@ export class SignicatLoanValidationComponent implements OnInit {
   @Output() confirm: boolean;
   @Output() redirect: boolean;
 
+  public loanTypeList: LoanTypeOption[] = nonListLoanType;
+
   public isMobile: boolean;
 
   constructor() {}
 
   ngOnInit(): void {
     window.innerWidth > 600 ? (this.isMobile = false) : (this.isMobile = true);
+    if (this.loans) {
+      this.loans.map((loan) => {
+        let convert: string | undefined = '';
+        convert = this.loanTypeList.find((val) => val.value === loan.loanType)
+          ?.name;
+
+        if (convert) loan.loanType = convert;
+        else console.log('Could not find loan type name', convert);
+      });
+    }
   }
 }
