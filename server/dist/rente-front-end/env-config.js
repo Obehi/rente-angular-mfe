4ff6e083592cdbd235a5c6d347ac8ca694e86c62
Obehi <stian.obehi@gmail.com>
2021-11-07 20:01:29 +0100

    dont know whats happening

--- server/dist/rente-front-end/env-config.js
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    name: 'local',
-    production: 'aaaargg',
-    baseUrl: 'https://rente-gateway-dev.herokuapp.com',
-    crawlerUrl: 'https://rente-ws-dev.herokuapp.com/ws',
-    tinkUrl: '',
-    locale: 'nb'
-  };
-  
